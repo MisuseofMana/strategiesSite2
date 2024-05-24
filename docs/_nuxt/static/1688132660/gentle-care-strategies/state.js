ab405c79e0bdceeb5826dec90b8ec04497d7bf69
Sean Yager <contact@seanyager.com>
2024-05-23 20:09:02 -0400

    deploying previous updates

--- docs/_nuxt/static/1688132660/gentle-care-strategies/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1688132660",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgentle-care-strategies",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
